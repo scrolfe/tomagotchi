bd25d4531c5526363193096b8247fe18f0c46e9c
Sam Rolfe <scrolfe@gmail.com>
2017-05-30 17:27:03 -0500

    added 2 functions, lots of offline buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,41 @@
 var cat = {
     name: "",
     attitudeLevel: 7,
-    meow: function (){
-        alert(cat.name + " says 'MEOWWW'")
+    kittyMeter: 5,
+    hunger: 7
+    stroke: function () {
+        if (kittyMeter===10) {
+            alert(this.name + " jumps 10 feets, kicks your face three times, and darts under the bed")
+            this.kittyMeter = 0
+            } else {
+            alert(this.name + " says 'MEOWWW'")
+            this.kittyMeter++
+        }
     },
     feed: function () {
-        alert(this.name + " says 'Yum!!'")
-        this.hunger = 0
+        if (this.hunger===0) {
+            alert(this.name + " runs under the bed!")
+        } else {
+            alert(this.name + " says 'Yum!!'")
+            this.kittyMeter = 0
+        }
     }
 };
 
 var catName = document.getElementById('catName')
+catName.addEventListener("click", function () {
+    cat.name = document.getElementById('nameInput').value
+    alert(cat.name + " says meow (hello) to user!")
+});
+//get these 2 to react to feeding, stroking, and playing
+var kittyMeter = document.getElementById('kittyMeter')
+var hunger = document.getElementById('hunger')
+
+//get these 3 to cause kittyMeter and hunger to go up or down
 var strokeButton = document.getElementById('stroke')
-var feedCat = document.getElementById('feed')
-var playWithCat = document.getElementById('play')
+var playButton = document.getElementById('play')
+
+strokeButton.addEventListener("click", function () {
+    cat.stroke()
+})
+var feedButton = document.getElementById('feed')
