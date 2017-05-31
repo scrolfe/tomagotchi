6784939536631e7f218bc9e239b7162d2b5bf388
Sam Rolfe <scrolfe@gmail.com>
2017-05-31 14:44:20 -0500

    clarified button structure, going to revert to jQuery now

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,62 @@
 var cat = {
     name: "",
-    attitudeLevel: 7,
     kittyMeter: 5,
-    hunger: 7
+    cozyMeter: 5,
+    hungerMeter: 5,
+    funMeter: 5,
+    happyCat: true,
     stroke: function () {
-        if (kittyMeter===10) {
-            alert(this.name + " jumps 10 feets, kicks your face three times, and darts under the bed")
-            this.kittyMeter = 0
-            } else {
-            alert(this.name + " says 'MEOWWW'")
-            this.kittyMeter++
+        if (cozyMeter > 5) {
+        } else {
         }
     },
     feed: function () {
-        if (this.hunger===0) {
-            alert(this.name + " runs under the bed!")
+        if (hungerMeter > 5) {
         } else {
-            alert(this.name + " says 'Yum!!'")
-            this.kittyMeter = 0
         }
+    },
+    play: function () {
+        if (funMeter > 5) {
+
+        } else {
     }
 };
 
+//appendNewElement function
+var body = document.body
+var appendNewElement = function (parent, parentIndex, element) {
+    var el = document.createElement(element)
+    document.getElementsByTagName(parent)[parentIndex]
+    body.appendChild(el)
+};
+//test
+appendNewElement("body", 0, "h1")
+//addInnerHtml function
+var addInnerHtml = function (location, text) {
+    //example use: adderInnerHtml('body h1', "I'm a cat, a cat, a cat!")
+    var parent = document.querySelectorAll(location)
+    parent.innerHTML(text)
+}
+//header function
+var header = function(size, text) {
+    if (size === "h1") {
+        //appendNewElement function
+        appendNewElement("body", 0, "h1")
+    } else if (size === "h2") {
+
+    } else if (size === "h3") {
+
+    } else {
+
+    }
+};
+//invoke name
 var catName = document.getElementById('catName')
 catName.addEventListener("click", function () {
     cat.name = document.getElementById('nameInput').value
     alert(cat.name + " says meow (hello) to user!")
 });
+
 //get these 2 to react to feeding, stroking, and playing
 var kittyMeter = document.getElementById('kittyMeter')
 var hunger = document.getElementById('hunger')
