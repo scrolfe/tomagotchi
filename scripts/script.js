2ea9465b8e1de0f31ef9c1f66801515d6602da7e
Sam Rolfe <scrolfe@gmail.com>
2017-06-02 16:38:32 -0500

    added response text to going over limits in kat kare kategories

--- scripts/script.js
+++ scripts/script.js
@@ -11,6 +11,7 @@ var kitty = {
             this.cozyMeter++
         } else {
             this.cozyMeter = 0
+            $('h1').text(this.name + " has been over stimulated by pets and pounces on your face! You die :(")
         }
         // console.log("hello")
     },
@@ -19,6 +20,7 @@ var kitty = {
             this.hungerMeter++
         } else {
             this.hungerMeter = 0
+            $('h1').text(this.name + " is so full.. Too full! " + this.name + " Takes a nap that never ends :(")
         }
     },
     play: function () {
@@ -26,6 +28,7 @@ var kitty = {
             this.funMeter++
         } else {
             this.funMeter = 0
+            $('h1').text(this.name + " is feeling far TOO playful and attacks your computer charger. Your computer crashes to the ground, exploding and starting a small fire.")
     }
 }}
 
@@ -33,7 +36,7 @@ $('#messageBox').text('Welcome to the KittyKareSenter')
 
 $('#submit').on('click', function () {
     kitty.name = $('#input').val()
-    $('#messageBox').text("Your kitty's name is " + kitty.name + ". Congratulations!! You are now a cat-sitter.")
+    $('#messageBox').text("Your kitty's name is " + kitty.name + ". Congratulations!! You are now an official cat-sitter.")
 })
 
 $('#stroke').on('click', function () {
